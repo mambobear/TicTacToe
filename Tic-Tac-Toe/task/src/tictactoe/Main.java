8b77c6140aff774604e0d9d3b324416bf0040759
andreizherebtsov <azherebtsov@gmail.com>
2021-02-12 15:06:44 -0800

    Finish TicTacToe

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -4,82 +4,124 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static char[][] grid = {
-            {'_', '_', '_'},
-            {'_', '_', '_'},
-            {'_', '_', '_'},
+    private static final char[][] grid = {
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
+            {' ', ' ', ' '},
     };
-    private static final int rows = 3;
-    private static final int cols = 3;
+    private static final int N = 3;
+    private static final char Empty = ' ';
 
-    private static char currentUser = 'X';
+    private static char currentPlayer = 'X';
 
     public static void main(String[] args) {
 
-        while (true) {
-            printGrid();
-            getUserInput();
-            
-        }
+        boolean done = false;
+        int[] move;
 
-//        readConfiguration();
-//        checkGrid();
-
-        getUserInput();
         printGrid();
+        while (!done) {
+            move = getUserMove();
+            printGrid();
+            done = checkMove(move);
+            switchPlayers();
+        }
+//        printGrid();
     }
 
-    private static void getUserInput() {
+    private static void switchPlayers() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    private static int[] getUserMove() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("Enter the coordinates: ");
             int row;
             int col;
             try {
-                row = Integer.parseInt(scanner.next());
-                col = Integer.parseInt(scanner.next());
+                String rowStr = scanner.next();
+                String colStr = scanner.next();
+                row = Integer.parseInt(rowStr);
+                col = Integer.parseInt(colStr);
 
                 if (row < 1 || row > 3 || col < 1 || col > 3) {
                     System.out.println("Coordinates should be from 1 to 3!");
                     continue;
                 }
-                if (grid[row - 1][col - 1] != '_') {
+                if (grid[row - 1][col - 1] != Empty) {
                     System.out.println("This cell is occupied! Choose another one!");
                     continue;
                 }
-                grid[row - 1][col - 1] = currentUser;
-                currentUser = (currentUser == 'X') ? 'O' : 'X';
-                break;
+                grid[row - 1][col - 1] = currentPlayer;
+                return new int[] {row - 1, col - 1};
             } catch (NumberFormatException e) {
                 System.out.println("You should enter numbers!");
             }
         }
     }
 
-    private static void checkGrid() {
-        boolean hasXRow = hasRowFor('X');
-        boolean hasORow = hasRowFor('O');
+    private static boolean checkMove(int[] move) {
+        boolean hasRow = hasRowFor(move);
 
-        int numFreeCells = numberOf('_');
-        int numX = numberOf('X');
-        int numO = numberOf('O');
+        int numFreeCells = numberOf(Empty);
 
-        if ((hasXRow && hasORow) || (Math.abs(numX - numO) >= 2)) {
-            System.out.println("Impossible");
-            return;
-        }
-
-        if (!hasXRow && !hasORow) {
+        if (!hasRow) {
             if (numFreeCells == 0) {
                 System.out.println("Draw");
-            } else {
-                System.out.println("Game not finished");
+                return true;
             }
-        } else if (!hasXRow){
-            System.out.println("O wins");
         } else {
-            System.out.println("X wins");
+            System.out.printf("%c wins\n", currentPlayer);
+            return true;
         }
+        return false;
+    }
+
+    private static boolean hasRowFor(int[] move) {
+
+        boolean fullRow = true;
+        for (int col = 0; col < N; col++) {
+            if (grid[move[0]][col] != currentPlayer)  {
+                fullRow = false;
+                break;
+            }
+        }
+        if (fullRow) return true;
+
+        fullRow = true;
+        for (int row = 0; row < N; row++) {
+            if (grid[row][move[1]] != currentPlayer)  {
+                fullRow = false;
+                break;
+            }
+        }
+        if (fullRow) return true;
+
+
+        if (move[0] == move[1]) {
+            fullRow = true;
+            for (int i = 0; i < N; i++) {
+                if (grid[i][i] != currentPlayer)  {
+                    fullRow = false;
+                    break;
+                }
+            }
+            if (fullRow) return true;
+        }
+
+        if (move[0] + move[1] == N - 1) {
+            fullRow = true;
+            for (int i = 0; i < N; i++) {
+                if (grid[i][N - 1 -i] != currentPlayer)  {
+                    fullRow = false;
+                    break;
+                }
+            }
+            if (fullRow) return true;
+        }
+
+        return false;
     }
 
     private static int numberOf(char x) {
@@ -90,54 +132,6 @@ public class Main {
             }
         }
         return count;
-    }
-
-    private static boolean hasRowFor(char x) {
-
-        boolean fullDiagMain = true;
-        boolean fullDiagSecond = true;
-
-        // Check rows and columns
-        for (int i = 0; i < 3; i++) {
-            boolean fullRow = true;
-            boolean fullCol = true;
-
-            for (int j = 0; j < 3; j++) {
-                if (grid[i][j] != x) {
-                    fullRow = false;
-                    break;
-                }
-            }
-
-            for (int j = 0; j < 3; j++) {
-                if (grid[j][i] != x) {
-                    fullCol = false;
-                    break;
-                }
-            }
-
-            if (fullRow || fullCol) return true;
-        }
-
-        // Check main diagonal
-        for (int i = 0; i < 3; i++) {
-            if(grid[i][i] != x) {
-                fullDiagMain = false;
-                break;
-            }
-        }
-        if (fullDiagMain) return true;
-
-        // Check second main diagonal
-        for (int i = 0; i < 3; i++) {
-            if(grid[i][2 - i] != x) {
-                fullDiagSecond = false;
-                break;
-            }
-        }
-        if (fullDiagSecond) return true;
-
-        return false;
     }
 
     private static void readConfiguration() {
